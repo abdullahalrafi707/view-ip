2e845d638dedc32ff4ac74e95c67ab1d50a7a2a3
Abdullah Al Rafi <92031886+abdullahalrafi707@users.noreply.github.com>
2023-06-15 17:12:29 +0600

    Add files via upload

---
+++ ip.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="Just Visit to View Your IP address !!!">
+    <title>IP Address</title>
+    <style>
+        body{
+            font-size: 35px;
+        }
+    </style>
+</head>
+<body>
+    <strong>Your IP address is : </strong> <?php echo $_SERVER["REMOTE_ADDR"]; ?>
+</body>
+</html>
